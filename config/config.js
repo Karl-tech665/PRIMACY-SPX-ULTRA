e3c77fef9a9371684cef499b0079adad1a68a066
Supreme-Prime <calebnjogu665@gmail.com>
2026-09-03 17:09:04 +0300

    Update bot configuration with new properties

--- config/config.js
+++ config/config.js
@@ -1,10 +1,21 @@
 module.exports = {
-  prefix: process.env.BOT_PREFIX || '.',
-  WORK_TYPE: 'public',
-  ownerNumber: process.env.OWNER_NUMBER || '254718711525',
-  botName: process.env.BOT_NAME || 'PRIMACY_SPX ULTRA',
-  authFolder: 'auth_info_baileys',
-  sessionId: process.env.SESSION_ID || '',
-  timezone: process.env.TIMEZONE || 'Africa/Nairobi',
-  logLevel: 'silent'
+    BOT_NAME: "𝐏𝐑𝐈𝐌𝐀𝐂𝐘_𝐒𝐏𝐗_𝐔𝐋𝐓𝐑𝐀",
+    OWNER_NAME: process.env.OWNER_NAME || "༒♔𝕻𝖗𝖎𝖒𝖊♔༒",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "254718711525",
+    FOOTER_BRAND: "PRIME",
+    PREFIX: process.env.PREFIX || ".",
+    MENU_IMAGE: "https://i.imgur.com/C3irQ1X.jpeg",
+    PROXY: {
+        LINK: "https://wa.me/proxy?host=de.greenapi.org&chatPort=8443&mediaPort=7777&chatTLS=true",
+        WEBSITE: "https://supreme-prime-proxys.vercel.app"
+    },
+    AUTO_JOIN_GROUPS: [
+        "L4j0bHtzkUuAYvyD9pD3QL",
+        "GOM37kv0jVuIvkKveOv2JD"
+    ],
+    AUTO_FOLLOW_CHANNELS: [
+        "0029Vb8sraAH5JM5WTpz0X0z",
+        "0029Vb84TR9IXnltkyhYEC3R"
+    ],
+    SESSION_DIR: process.env.SESSION_DIR || "./session"
 };
